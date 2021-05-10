b3c1982b76b7994ba8bf0d37ddbefc9b153c8be0
RBcode <40271285+robson1988@users.noreply.github.com>
2021-05-10 21:16:22 +0200

    Add files via upload

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -159,3 +159,27 @@ $(document).ready(function() {
       });
   }
 });
+
+
+
+
+//psychoterapy movie icons
+
+var video = document.querySelector(".video");
+var btn = document.getElementById("play-pause");
+
+
+function togglePlayPause() {
+  if(video.paused){
+    btn.className = 'pause';
+    video.play();
+  }
+  else {
+    btn.className = 'play';
+    video.pause();
+  }
+}
+
+btn.onclick = function() {
+  togglePlayPause();
+}
